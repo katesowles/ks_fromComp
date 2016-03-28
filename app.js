3d9b16700f4dfe9c54a31f894dc639802b67d073
goodwid <goodwid@gmail.com>
2016-03-28 16:29:23 -0700

    Commented out button for JS code.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var ingred = document.getElementsByClassName('ingredients')[0];
 var listButton = document.getElementById('listbutton');
 
 listButton.addEventListener("click", function () {
-    var checkboxes = document.querySelectorAll('input:checked');
+    var checkboxes = document.querySelectorAll('input:checked')[0];
     console.log(checkboxes);
 });
 
