a765d885106ab2095c486f6964e59d92e115fd62
goodwid <goodwid@gmail.com>
2016-03-28 16:11:14 -0700

    Script update

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-console.log('loaded');
 
 var ingred = document.getElementsByClassName('ingredients')[0];
 var listButton = document.getElementById('listbutton');
 
 listButton.addEventListener("click", function () {
-    console.log('clicked');
     var checkboxes = document.querySelectorAll('input:checked');
     console.log(checkboxes);
 });
